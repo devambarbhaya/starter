2503663c6dae713f4bb312cce8330f0f6f4db496
Devam Barbahya <barbhaya.devam@gmail.com>
2024-09-01 13:24:39 +0530

    add Project Section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,46 +1,151 @@
-import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
-import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
-import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import Image from "next/image";
+import disclosure from "@/assets/disclosure.png";
+import lingo from "@/assets/lingo.png";
+import shoeMarshal from "@/assets/shoe-marshal.png";
+import figma from "@/assets/figma.png";
+import gericht from "@/assets/gericht.png";
+import gpt from "@/assets/gpt.png";
+import techfest from "@/assets/techfest.png";
+import moodIndigo from "@/assets/mood-indigo.png";
+import CheckIcon from "@/assets/icons/check-circle.svg";
+import ArrowUpRight from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
 
 const portfolioProjects = [
   {
-    company: "Acme Corp",
-    year: "2022",
-    title: "Dark Saas Landing Page",
-    results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
-    ],
-    link: "https://youtu.be/4k7IdSLxh6w",
-    image: darkSaasLandingPage,
+    company: "TSS Consultancy",
+    year: "2024",
+    title: "Disclosure360",
+    results: [{ title: "1" }, { title: "2" }, { title: "3" }],
+    link: "https://disclosure360.in/",
+    image: disclosure,
   },
   {
-    company: "Innovative Co",
-    year: "2021",
-    title: "Light Saas Landing Page",
-    results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
-    ],
-    link: "https://youtu.be/7hi5zwO75yc",
-    image: lightSaasLandingPage,
+    company: "Techfest, IIT Bombay",
+    year: "2024",
+    title: "Competitions Manager",
+    results: [{ title: "1" }, { title: "2" }, { title: "3" }],
+    link: "https://techfest.org/",
+    image: techfest,
   },
   {
-    company: "Quantum Dynamics",
+    company: "Personal Project",
+    year: "2024",
+    title: "Lingo",
+    results: [{ title: "1" }, { title: "2" }, { title: "3" }],
+    link: "https://lingo-devam.vercel.app/",
+    image: lingo,
+  },
+  {
+    company: "Personal Project",
     year: "2023",
-    title: "AI Startup Landing Page",
-    results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
-    ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
-    image: aiStartupLandingPage,
+    title: "Shoe Marshal",
+    results: [{ title: "1" }, { title: "2" }, { title: "3" }],
+    link: "https://shoe-marshal-devam.vercel.app/",
+    image: shoeMarshal,
+  },
+  {
+    company: "Personal Project",
+    year: "2023",
+    title: "FigPro",
+    results: [{ title: "1" }, { title: "2" }, { title: "3" }],
+    link: "https://figma-clone-phi.vercel.app/",
+    image: figma,
+  },
+  {
+    company: "Personal Project",
+    year: "2023",
+    title: "Gericht Restaurant",
+    results: [{ title: "1" }, { title: "2" }, { title: "3" }],
+    link: "https://devambarbhaya.github.io/gericht-restaurant/",
+    image: gericht,
+  },
+  {
+    company: "Personal Project",
+    year: "2022",
+    title: "GPT-3 Explain",
+    results: [{ title: "1" }, { title: "2" }, { title: "3" }],
+    link: "https://devambarbhaya.github.io/gpt-3-explain/",
+    image: gpt,
+  },
+  {
+    company: "Mood Indigo",
+    year: "2022",
+    title: "Events Manager",
+    results: [{ title: "1" }, { title: "2" }, { title: "3" }],
+    link: "https://moodi.org/",
+    image: moodIndigo,
   },
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return (
+    <section className="pb-16">
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent text-center">
+            Real-World Results
+          </p>
+        </div>
+        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
+          Featured Projects
+        </h2>
+        <p className="text-center text-white/60 mt-4 max-w-md mx-auto md:text-lg lg:text-xl">
+          See how I transformed concepts into engaging digital experiences.
+        </p>
+        <div className="flex flex-col mt-10 gap-20 md:mt-20">
+          {portfolioProjects.map((project, index) => (
+            <div
+              key={index}
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:mt-16 lg:px-20 after:pointer-events-none"
+            >
+              <div
+                className="absolute inset-0 -z-10 opacity-5"
+                style={{
+                  backgroundImage: `url(${grainImage.src})`,
+                }}
+              ></div>
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex font-bold uppercase tracking-widest text-sm gap-2 text-transparent bg-clip-text">
+                    <span>{project.company}</span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
+                  <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">
+                    {project.title}
+                  </h3>
+                  <hr className="border-t-2 border-white/5 mt-4" />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result, index) => (
+                      <li
+                        key={index}
+                        className="flex gap-2 text-sm md:text-base text-white/50"
+                      >
+                        <CheckIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <a href={project.link} target="_blank">
+                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                      <span>Visit Live Site</span>
+                      <ArrowUpRight className="size-4" />
+                    </button>
+                  </a>
+                </div>
+                <div className="relative">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
