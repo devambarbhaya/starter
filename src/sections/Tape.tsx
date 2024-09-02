f42b38c9070b6a5f1d4e1098c786d49ef4519fa0
Devam Barbahya <barbhaya.devam@gmail.com>
2024-09-02 20:51:09 +0530

    add Tape Section Animations

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,4 +1,5 @@
 import StarIcon from "@/assets/icons/star.svg";
+import { Fragment } from "react";
 
 const words = [
   "Performant",
@@ -20,14 +21,18 @@ export const TapeSection = () => {
     <div className="py-16 lg:py-24  overflow-x-clip">
       <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex flex-none gap-4 py-3">
-            {words.map((word, index) => (
-              <div key={index} className="inline-flex gap-4 items-center">
-                <span className="text-gray-900 uppercase font-extrabold text-sm">
-                  {word}
-                </span>
-                <StarIcon className="size-6 text-gray-900 -rotate-12" />
-              </div>
+          <div className="flex flex-none gap-4 pr-4 py-3 animate-move-left [animation-duration:45s]">
+            {[...new Array(2)].fill(0).map((_, index) => (
+              <Fragment key={index}>
+                {words.map((word, index) => (
+                  <div key={index} className="inline-flex gap-4 items-center">
+                    <span className="text-gray-900 uppercase font-extrabold text-sm">
+                      {word}
+                    </span>
+                    <StarIcon className="size-6 text-gray-900 -rotate-12" />
+                  </div>
+                ))}
+              </Fragment>
             ))}
           </div>
         </div>
