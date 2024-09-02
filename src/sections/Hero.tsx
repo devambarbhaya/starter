b9b1643e1cbafb5afa8a2149fa7f999e00a9859f
Devam Barbahya <barbhaya.devam@gmail.com>
2024-09-03 01:23:38 +0530

    add Hero Changes

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -140,14 +140,18 @@ export const HeroSection = ({ id }: HeroProps) => {
           </div>
 
           <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
-            <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
-              <span className="font-semibold">Explore my work</span>
-              <ArrowDown className="size-4" />
-            </button>
-            <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
-              <span>👋</span>
-              <span className="font-semibold">Let&apos;s Connect!</span>
-            </button>
+            <a href="#projects" className="z-20">
+              <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+                <span className="font-semibold">Explore my work</span>
+                <ArrowDown className="size-4" />
+              </button>
+            </a>
+            <a href="#contact" className="z-20">
+              <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+                <span>👋</span>
+                <span className="font-semibold">Let&apos;s Connect!</span>
+              </button>
+            </a>
           </div>
         </div>
       </div>
