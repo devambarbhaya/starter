a9d385a7e59e00051c4cc079149e18595af1c2ce
Devam Barbahya <barbhaya.devam@gmail.com>
2024-09-02 15:33:44 +0530

    add Footer Section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,54 @@
+import ArrowUpRight from "@/assets/icons/arrow-up-right.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+import LinkedInIcon from "@/assets/icons/linkedin.svg";
+import InstagramIcon from "@/assets/icons/instagram.svg";
+import TwitterIcon from "@/assets/icons/twitter.svg";
+
+const footerLinks = [
+  {
+    title: "Github",
+    href: "https://github.com/devambarbhaya",
+    Icon: GithubIcon,
+  },
+  {
+    title: "LinkedIn",
+    href: "https://www.linkedin.com/in/devam-barbhaya/",
+    Icon: LinkedInIcon,
+  },
+  {
+    title: "Instagram",
+    href: "https://www.instagram.com/devambarbhaya/",
+    Icon: InstagramIcon,
+  },
+  {
+    title: "Twitter",
+    href: "https://x.com/BarbhayaDevam",
+    Icon: TwitterIcon,
+  },
+];
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer className="relative -z-10 overflow-x-clip">
+      <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
+      <div className="container">
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
+          <div className="text-white/40">&copy; 2024. All rights reserved</div>
+          <nav className="flex flex-col md:flex-row items-center gap-8">
+            {footerLinks.map((link, index) => (
+              <a
+                href={link.href}
+                key={index}
+                className="inline-flex items-center gap-1.5"
+              >
+                <link.Icon className="size-4" />
+                <span className="font-semibold">{link.title}</span>
+                <ArrowUpRight className="size-4" />
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 };
