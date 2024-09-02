3d1a5d8fa0e72c7f0f0bca7f1f3caec89473d0e5
Devam Barbahya <barbhaya.devam@gmail.com>
2024-09-03 01:17:40 +0530

    add Projects Changes

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -115,7 +115,7 @@ export const ProjectsSection = ({ id }: ProjectsProps) => {
                       <span>&bull;</span>
                       <span>{project.year}</span>
                     </div>
-                    <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">
+                    <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-3xl">
                       {project.title}
                     </h3>
                     <hr className="border-t-2 border-white/5 mt-4" />
